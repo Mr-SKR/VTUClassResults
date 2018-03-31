2e3d5f4ce083d7a1d7788650923e565045afaaa9
Suresh Kumar Reddy <sure14is@cmrit.ac.in>
2018-04-01 00:42:37 +0530

    Added Headless Chrome options
    The program now runs in background without no visibility to the user

--- src/Results.java
+++ src/Results.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class Results {
@@ -21,8 +22,10 @@ public class Results {
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(chromeOptions);
 		
-		WebDriver driver = new ChromeDriver();
 		driver.manage().window().fullscreen();
 		
 		ArrayList<String> al = new ArrayList<String>();
